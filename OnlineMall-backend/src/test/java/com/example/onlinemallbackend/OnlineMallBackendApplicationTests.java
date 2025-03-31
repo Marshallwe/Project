f4c22a8dce0dce4b48e7f77b7a4951fb045a76cc
Marshall <mao.chen@student.zy.cdut.edu.cn>
2025-03-31 20:12:19 +0800

    test

---
+++ OnlineMall-backend/src/test/java/com/example/onlinemallbackend/OnlineMallBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinemallbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineMallBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
